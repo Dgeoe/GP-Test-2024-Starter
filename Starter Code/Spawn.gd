b34976deb4ef736bbe177d779cac5bad3572bf6c
Dgeoe <joedoshea15@gmail.com>
2024-05-02 15:19:47 +0100

    audiostreamplayer working on button pressed

--- Starter Code/Spawn.gd
+++ Starter Code/Spawn.gd
@@ -1,7 +1,10 @@
 extends Button
 
+var audio_player: AudioStreamPlayer2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	audio_player = $AudioStreamPlayer2D
 	var button = Button.new()
 	button.text = "============================================
 	SACRIFICE! 
@@ -18,6 +21,21 @@ func _button_pressed():
 	var instance = scene.instantiate()
 	instance.position = Vector2(X, Y)
 	add_child(instance)
+	
+	#Play sound
+	if audio_player:
+		# Generate a random pitch 
+		var random_pitch = generate_random_pitch()
+		
+		# Set the pitch 
+		audio_player.pitch_scale = random_pitch
+		
+		# Play the audio
+		audio_player.play()
+	else:
+		print("AudioStreamPlayer2D node not found or invalid.")
+	
+	#Delete attempt
 	var currentPOS = instance.position.y
 	if currentPOS >= 640: 
 		print("delete here")
@@ -29,6 +47,12 @@ func _process(delta):
 	pass
 	
 
+# pitch generator functon
+func generate_random_pitch():
+	var min_pitch = 0.8
+	var max_pitch = 1.2
+	
+	return randf_range(min_pitch, max_pitch)
 
 	
 	
