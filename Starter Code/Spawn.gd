e635ff59c94fc72c8027eda1bc0f3d2a513743c1
Dgeoe <joedoshea15@gmail.com>
2024-05-02 14:54:52 +0100

    randomized spawning

--- Starter Code/Spawn.gd
+++ Starter Code/Spawn.gd
@@ -9,19 +9,27 @@ func _ready():
 	button.pressed.connect(self._button_pressed)
 	add_child(button)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	
 func _button_pressed():
+	var X = randi() % 500
+	var Y = randi() % 500
 	print("Hello world!")
 	var scene = preload("res://Obj2bspawned.tscn")
 	var instance = scene.instantiate()
+	instance.position = Vector2(X, Y)
 	add_child(instance)
 	var currentPOS = instance.position.y
 	if currentPOS >= 640: 
 		print("delete here")
 		instance.queue_free()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
+
+	
 	
 	
 
